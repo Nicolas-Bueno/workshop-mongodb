11a26a83956051641234fff7e16c93abc772ecf9
Nicolas Bueno <nicolasbrian202@gmail.com>
2024-07-15 10:55:50 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nb/workshopmongo/domain/User.java
+++ src/main/java/com/nb/workshopmongo/domain/User.java
@@ -1,7 +1,12 @@
 package com.nb.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User {
 
+    @Id
     private String id;
     private String name;
     private String email;
